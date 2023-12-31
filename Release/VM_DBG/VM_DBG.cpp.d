e88475e99947d9e8fb73d2b36a2956d8417b3edf
Kevin Guest <github@bionicbone.plus.com>
2023-12-31 08:17:54 +0000

    Upload a Release version

---
+++ Release/VM_DBG/VM_DBG.cpp.d
@@ -0,0 +1,15 @@
+C:\Users\Micro\AppData\Local\Temp\VMBuilds\433mhz_Rolling_Code\nano_atmega328old\Release\VM_DBG\VM_DBG.cpp.o: \
+ c:\program\ files\ (x86)\microsoft\ visual\ studio\2019\community\common7\ide\extensions\mz4xesrp.2la\Micro\ Platforms\default\debuggers\VM_DBG\VM_DBG.cpp \
+ c:\program\ files\ (x86)\microsoft\ visual\ studio\2019\community\common7\ide\extensions\mz4xesrp.2la\Micro\ Platforms\default\debuggers\VM_DBG\VM_DBG.h \
+ C:\Users\Micro\AppData\Local\arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino/Arduino.h \
+ C:\Users\Micro\AppData\Local\arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino/binary.h \
+ C:\Users\Micro\AppData\Local\arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino/WCharacter.h \
+ C:\Users\Micro\AppData\Local\arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino/WString.h \
+ C:\Users\Micro\AppData\Local\arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino/HardwareSerial.h \
+ C:\Users\Micro\AppData\Local\arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino/Stream.h \
+ C:\Users\Micro\AppData\Local\arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino/Print.h \
+ C:\Users\Micro\AppData\Local\arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino/Printable.h \
+ C:\Users\Micro\AppData\Local\arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino/USBAPI.h \
+ C:\Users\Micro\AppData\Local\arduino15\packages\arduino\hardware\avr\1.8.5\cores\arduino/Arduino.h \
+ C:\Users\Micro\AppData\Local\arduino15\packages\arduino\hardware\avr\1.8.5\variants\eightanaloginputs/pins_arduino.h \
+ c:\users\micro\appdata\local\arduino15\packages\arduino\hardware\avr\1.8.5\variants\standard\pins_arduino.h
